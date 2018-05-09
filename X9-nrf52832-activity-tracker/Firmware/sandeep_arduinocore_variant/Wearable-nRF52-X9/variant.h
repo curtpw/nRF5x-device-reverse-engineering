6edb0623fd1f5dfec85bb9d9426fb2a31102f1f3
Curt White <curt3n5@gmail.com>
2018-05-09 15:59:34 -0400

    rename and clarify configuration of SPI GPIO in X9 variant file

--- X9-nrf52832-activity-tracker/Firmware/sandeep_arduinocore_variant/Wearable-nRF52-X9/variant.h
+++ X9-nrf52832-activity-tracker/Firmware/sandeep_arduinocore_variant/Wearable-nRF52-X9/variant.h
@@ -12,10 +12,48 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  ADD THE FOLLOWING TO boards.txt:
+  
+Wearable-nRF52-X9.name=Wearable nrf52 X9
+
+Wearable-nRF52-X9.upload.tool=sandeepmistry:openocd
+Wearable-nRF52-X9.upload.target=nrf52
+Wearable-nRF52-X9.upload.maximum_size=524288
+
+Wearable-nRF52-X9.bootloader.tool=sandeepmistry:openocd
+
+Wearable-nRF52-X9.build.mcu=cortex-m4
+Wearable-nRF52-X9.build.f_cpu=16000000
+Wearable-nRF52-X9.build.board=WEARABLE_NRF52_X9
+Wearable-nRF52-X9.build.core=nRF5
+Wearable-nRF52-X9.build.variant=Wearable-nRF52-X9
+Wearable-nRF52-X9.build.variant_system_lib=
+Wearable-nRF52-X9.build.extra_flags=-DNRF52
+Wearable-nRF52-X9.build.float_flags=-mfloat-abi=hard -mfpu=fpv4-sp-d16
+Wearable-nRF52-X9.build.ldscript=nrf52_xxaa.ld
+
+Wearable-nRF52-X9.menu.softdevice.none=None
+Wearable-nRF52-X9.menu.softdevice.none.softdevice=none
+
+Wearable-nRF52-X9.menu.softdevice.s132=S132
+Wearable-nRF52-X9.menu.softdevice.s132.softdevice=s132
+Wearable-nRF52-X9.menu.softdevice.s132.softdeviceversion=2.0.1
+Wearable-nRF52-X9.menu.softdevice.s132.upload.maximum_size=409600
+Wearable-nRF52-X9.menu.softdevice.s132.build.extra_flags=-DNRF52 -DS132 -DNRF51_S132
+Wearable-nRF52-X9.menu.softdevice.s132.build.ldscript=armgcc_s132_nrf52832_xxaa.ld
+
+Wearable-nRF52-X9.menu.lfclk.lfxo=Crystal Oscillator
+Wearable-nRF52-X9.menu.lfclk.lfxo.build.lfclk_flags=-DUSE_LFXO
+Wearable-nRF52-X9.menu.lfclk.lfrc=RC Oscillator
+Wearable-nRF52-X9.menu.lfclk.lfrc.build.lfclk_flags=-DUSE_LFRC
+Wearable-nRF52-X9.menu.lfclk.lfsynt=Synthesized
+Wearable-nRF52-X9.menu.lfclk.lfsynt.build.lfclk_flags=-DUSE_LFSYNT
 */
+
 #define NRF52
-#ifndef _VARIANT_CHILDMIND_X9_
-#define _VARIANT_CHILDMIND_X9_
+#ifndef _VARIANT_WEARABLE_NRF52_X9_
+#define _VARIANT_WEARABLE_NRF52_X9_
 
 /** Master clock frequency */
 #ifdef NRF52
@@ -74,20 +112,41 @@ static const uint8_t A7  = PIN_A7 ;
 /*
  * Serial interfaces
  */
-// Serial
-#define PIN_SERIAL_RX       (14)
+
+// Serial Cannibalized from OLED
+#define PIN_SERIAL_RX       (14) 
 #define PIN_SERIAL_TX       (13)
+
+// Serial dummy NC GPIO
+//#define PIN_SERIAL_RX       (25) 
+//#define PIN_SERIAL_TX       (26)
 
 /*
  * SPI Interfaces
+ * IMPORTANT: The accelerometer/flash and the OLED are on seperate SPI interfaces ie separate GPIO.
+ * It is possible to have two SPI interfaces working at once, but this is not currently supported 
+ * by Sandeep's Nordic ArduinoCore SPI library. I will write a modified SPI library that supports
+ * two interfaces as soon as I can, but for now you must choose between the accelerometer and display.
+ * Comment out the SPI GPIO and select GPIO for the SPI device you choose not to use.
  */
+
 #define SPI_INTERFACES_COUNT 1
 
+//SPI GPIO for KX126 Accelerometer
+/*
 #define PIN_SPI_MISO         (20)
 #define PIN_SPI_MOSI         (19)
 #define PIN_SPI_SCK          (18)
+static const uint8_t SS     = 24 ;
+*/
 
-static const uint8_t SS   = 24 ;
+
+//SPI for 96x64 Color OLED
+#define PIN_SPI_MISO         (13)
+#define PIN_SPI_MOSI         (11)
+#define PIN_SPI_SCK          (12)
+static const uint8_t SS     = 15 ;
+
 static const uint8_t MOSI = PIN_SPI_MOSI ;
 static const uint8_t MISO = PIN_SPI_MISO ;
 static const uint8_t SCK  = PIN_SPI_SCK ;
